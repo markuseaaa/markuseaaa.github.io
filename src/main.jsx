770b8eb39af8f4759ebaecde312465a9eace5c9f
markuseaaa <eaa24markuk@students.eaaa.dk>
2025-09-03 08:19:10 +0200

    Browserrouter 2

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter
-      basename={import.meta.env.DEV ? "/" : "/YOUR-REPOSITORY-NAME/"}
+      basename={import.meta.env.DEV ? "/" : "/markuseaaa.github.io/"}
     >
       <App />
     </BrowserRouter>
